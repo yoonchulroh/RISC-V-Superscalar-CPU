c299f343559a81e7c8099d67147d28d889f5476c
yoonchulroh <60857530+yoonchulroh@users.noreply.github.com>
2026-03-25 15:22:12 +0900

    directory name change in verilator script

--- inst/verilator.sh
+++ inst/verilator.sh
@@ -1,6 +1,6 @@
 NAME=${1:-code}
 ./assemble.sh $NAME
 
-verilator --cc --exe --build -j 0 -y ../RISC-V.srcs -Wno-WIDTH sim.cpp top_for_verilator.v
+verilator --cc --exe --build -j 0 -y ../srcs -Wno-WIDTH sim.cpp top_for_verilator.v
 ./obj_dir/Vtop_for_verilator main.hex
 echo -e "\n\n"
